4046847154e6e011cd09afa612fae7dfb21d3a2a
Carlos Jovel <cm.jovel@gmail.com>
2015-04-29 09:25:30 -0600

    add sidebar tmp

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,9 @@ $(function(){
 		e.preventDefault();
 		$('.section-user-products').toggleClass('active');
 	});
+  $('.close-sidebar').on('click', function(e){
+    e.preventDefault();
+    $('.content-wrapper').animate({width: '100%'});
+    $('.sidebar-wrapper').animate({opacity: 0});
+  });
 });
