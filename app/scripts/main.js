89dedb9c1a262a652f811417b0e38d7d84cddb15
Carlos Jovel <cm.jovel@gmail.com>
2015-04-29 11:31:30 -0600

    styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,5 +9,18 @@ $(function(){
     e.preventDefault();
     $('.content-wrapper').animate({width: '100%'});
     $('.sidebar-wrapper').animate({opacity: 0});
+    $('.search-input').removeClass('office-open');
+    setTimeout(function(){
+      $('.item-office').show().animate({opacity: 1}, 200);
+    }, 400);
+  });
+  $('.item-office').on('click', function(e) {
+    e.preventDefault();
+    $('.content-wrapper').animate({width: '80%'});
+    $('.sidebar-wrapper').animate({opacity: 1});
+    $(this).animate({opacity: 0}, 400, function(){
+      $('.item-office').hide();
+      $('.search-input').addClass('office-open');
+    });
   });
 });
