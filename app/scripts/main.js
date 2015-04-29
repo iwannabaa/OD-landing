0ecf31192e5386e473fa4f23524100d171bcb971
Carlos Jovel <cm.jovel@gmail.com>
2015-04-29 02:37:59 -0600

    add sections and sidebar. todo: sidebar sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(function(){
+	$('.toggle-content').on('click', function(e) {
+		e.preventDefault();
+		$('.section-user-products').toggleClass('active');
+	});
+});
